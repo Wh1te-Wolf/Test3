4f6c7b5713d4a6a72e140d6a88bdb7e1ea4636d2
Антон Зубаков <lRevanI@yandex.ru>
2024-09-05 23:17:09 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Test3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var chars = new[] { 'A', 'B', 'C' };
+            var sequences = StringPermutations.GetAllSequences(chars);
+
+            foreach (var sequence in sequences)
+            {
+                Console.WriteLine(sequence);
+            }
+        }
+    }
+}
---
+++ StringPermutations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test3
+{
+    public class StringPermutations
+    {
+        public static IEnumerable<string> GetAllSequences(IEnumerable<char> chars)
+        {
+            if (!chars.Any())
+            {
+                yield return string.Empty;
+            }
+            else
+            {
+                for (int i = 0; i < chars.Count(); i++)
+                {
+                    var currentChar = chars.ElementAt(i);
+                    var remainingChars = chars.Where((c, index) => index != i);
+
+                    foreach (var subSequence in GetAllSequences(remainingChars))
+                    {
+                        yield return currentChar + subSequence;
+                    }
+                }
+            }
+        }
+    }
+}
